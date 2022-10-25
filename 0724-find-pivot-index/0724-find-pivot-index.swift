af7ed09258ebd472c87e61c4de75cacef377c706
CheonSong <qkrcjsthd@gmail.com>
2022-10-25 14:05:49 +0900

    Time: 823 ms (5.44%), Space: 14.2 MB (88.33%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.swift
+++ 0724-find-pivot-index/0724-find-pivot-index.swift
@@ -1,11 +1,12 @@
 class Solution {
     func pivotIndex(_ nums: [Int]) -> Int {
         var answer = -1
+        let sum = nums.reduce(0, +)
         
         for i in 0..<nums.count {
             var left = i == 0 ? 0 : Array(nums[0..<i]).reduce(0, +)
-            var right = Array(nums[i+1..<nums.count]).reduce(0, +)
-            if left == right {
+            
+            if left == sum-left-nums[i] {
                 answer = i
                 break
             }
