9c4a2fc28bb0cd626de43bb9c2b8cabeff41b963
CheonSong <qkrcjsthd@gmail.com>
2022-10-31 13:17:23 +0900

    Time: 811 ms (13.31%), Space: 14.5 MB (31.06%) - LeetHub

--- 0704-binary-search/0704-binary-search.swift
+++ 0704-binary-search/0704-binary-search.swift
@@ -1,8 +1,6 @@
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
         if !nums.contains(target) { return -1 }
-        if nums.first == target { return 0 }
-        if nums.last == target { return nums.count - 1 }
         
         return nums.firstIndex(of: target)!
     }
